6874d36d30043eee0f73f32900470218e19ebc16
memesoundboard <50186369+absolute-music@users.noreply.github.com>
2019-11-12 23:06:41 -0800

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,14 +1,9 @@
 const Client = require('./Client');
 
 // Platforms
-Client.battelenet = '6';
+Client.psn = '6';
 Client.xbox = '1';
 Client.ps4 = '2';
 
-// Game
-Client.bo4 = 'bo4';
-Client.iw = 'iw';
-Client.bo3 = 'bo3';
-Client.wwii = 'wwii';
 
 module.exports = Client;
